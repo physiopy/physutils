52cc61e8bb525cb141b549e4d664a69b0a8fb706
maestroque <kikasgeorg@ece.auth.gr>
2024-07-22 16:53:37 +0300

    Update pydra task

--- physutils/tasks.py
+++ physutils/tasks.py
@@ -1,10 +1,18 @@
 import pydra
 
-from physutils.io import load_physio
+from physutils.io import load_history, load_physio
 from physutils.physio import Physio
 
 
 @pydra.mark.task
-def transform_to_physio(input_file: str) -> Physio:
-    physio_obj = load_physio(input_file)
+def transform_to_physio(input_file: str, mode="physio") -> Physio:
+    if mode == "physio":
+        physio_obj = load_physio(input_file)
+    elif mode == "history":
+        physio_obj = load_history(input_file)
+    elif mode == "bids":
+        # TODO: Implement BIDS loading once the bids-support branch is merged
+        raise NotImplementedError("BIDS loading is not yet implemented")
+    else:
+        raise ValueError(f"Invalid transform_to_physio mode: {mode}")
     return physio_obj
